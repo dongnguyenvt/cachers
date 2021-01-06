af5c97c0b1888dd4f11f6ff43db2651fb816a3a5
Dong Nguyen <nguyenduydong@gmail.com>
2021-01-06 14:45:44 +0700

    add path and graceful shutdown

--- src/main.rs
+++ src/main.rs
@@ -2,7 +2,8 @@ use std::convert::Infallible;
 use std::net::SocketAddr;
 use hyper::{Body, Request, Response, Server};
 use hyper::service::{make_service_fn, service_fn};
-use tokio::io::AsyncWriteExt;
+// use tokio::io::AsyncWriteExt;
+use hyper::{Method, StatusCode};
 
 #[tokio::main]
 async fn main() {
@@ -10,20 +11,42 @@ async fn main() {
     let addr = SocketAddr::from(([127, 0, 0, 1], 3000));
 
     // A `Service` is needed for every connection, so this
-    // creates one from our `hello_world` function.
+    // creates one from our `handle` function.
     let make_svc = make_service_fn(|_conn| async {
         // service_fn converts our function into a `Service`
-        Ok::<_, Infallible>(service_fn(hello_world))
+        Ok::<_, Infallible>(service_fn(handle))
     });
 
     let server = Server::bind(&addr).serve(make_svc);
+    let graceful = server.with_graceful_shutdown(shutdown_signal());
 
     // Run this server for... forever!
-    if let Err(e) = server.await {
+    if let Err(e) = graceful.await {
         eprintln!("server error: {}", e);
     }
 }
 
-async fn hello_world(_req: Request<Body>) -> Result<Response<Body>, Infallible> {
-    Ok(Response::new("Hello, World".into()))
+async fn handle(req: Request<Body>) -> Result<Response<Body>, Infallible> {
+    let mut response = Response::new(Body::empty());
+
+    match (req.method(), req.uri().path()) {
+        (&Method::GET, "/ping") => {
+            *response.body_mut() = Body::from("hello world\n");
+        }
+        (&Method::GET, "/download") => {
+            *response.body_mut() = Body::from("download\n");
+        }
+        _ => {
+            *response.status_mut() = StatusCode::NOT_FOUND;
+        }
+    };
+
+    Ok(response)
+}
+
+async fn shutdown_signal() {
+    // Wait for the CTRL+C signal
+    tokio::signal::ctrl_c()
+        .await
+        .expect("failed to install CTRL+C signal handler");
 }
