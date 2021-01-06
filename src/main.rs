2b96963a1cec9388ed1033a732980b51f80fa35e
Dong Nguyen <nguyenduydong@gmail.com>
2021-01-06 18:01:14 +0700

    actix

--- src/main.rs
+++ src/main.rs
@@ -1,52 +1,23 @@
-use std::convert::Infallible;
-use std::net::SocketAddr;
-use hyper::{Body, Request, Response, Server};
-use hyper::service::{make_service_fn, service_fn};
-// use tokio::io::AsyncWriteExt;
-use hyper::{Method, StatusCode};
+use actix_web::{get, App, HttpRequest, HttpServer, Responder};
 
-#[tokio::main]
-async fn main() {
-    // We'll bind to 127.0.0.1:3000
-    let addr = SocketAddr::from(([127, 0, 0, 1], 3000));
-
-    // A `Service` is needed for every connection, so this
-    // creates one from our `handle` function.
-    let make_svc = make_service_fn(|_conn| async {
-        // service_fn converts our function into a `Service`
-        Ok::<_, Infallible>(service_fn(handle))
-    });
-
-    let server = Server::bind(&addr).serve(make_svc);
-    let graceful = server.with_graceful_shutdown(shutdown_signal());
-
-    // Run this server for... forever!
-    if let Err(e) = graceful.await {
-        eprintln!("server error: {}", e);
-    }
+#[get("/ping")]
+async fn ping(_req: HttpRequest) -> impl Responder {
+    "hello world\n"
 }
 
-async fn handle(req: Request<Body>) -> Result<Response<Body>, Infallible> {
-    let mut response = Response::new(Body::empty());
-
-    match (req.method(), req.uri().path()) {
-        (&Method::GET, "/ping") => {
-            *response.body_mut() = Body::from("hello world\n");
-        }
-        (&Method::GET, "/download") => {
-            *response.body_mut() = Body::from("download\n");
-        }
-        _ => {
-            *response.status_mut() = StatusCode::NOT_FOUND;
-        }
-    };
-
-    Ok(response)
+#[get("/download")]
+async fn download(_req: HttpRequest) -> impl Responder {
+    "download\n"
 }
 
-async fn shutdown_signal() {
-    // Wait for the CTRL+C signal
-    tokio::signal::ctrl_c()
+#[actix_web::main]
+async fn main() -> std::io::Result<()> {
+    HttpServer::new(|| {
+        App::new()
+            .service(ping)
+            .service(download)
+    })
+        .bind(("127.0.0.1", 8080))?
+        .run()
         .await
-        .expect("failed to install CTRL+C signal handler");
 }
